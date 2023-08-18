06c487013ea0571b9de5529c2b84750f1abbccd0
AlbertXiaoLin <linzhengyu1998@foxmail.com>
2023-08-18 22:37:51 +0800

    anything

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,8 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+
+fn quitcall_me() {
+    
+}
 fn main() {
-    call_me();
+    quitcall_me();
 }
